5b37525adf42fe71b0328349284d68c883d79858
JanClodMamDral <lavrinenko.lesha34@mail.ru>
2026-09-06 23:17:34 +0700

    Первый коммит: меню и функция варианта 20

---
+++ main.cpp
@@ -0,0 +1,46 @@
+//Лабораторная работа № 1. Вариант 20.
+//Выполнил Лавриненко А.П. , группа ПИ-54.
+#include <iostream>
+using namespace std;
+
+//===== Расчётные функции варианта =====
+
+//Объём пирамиды с квадратным основанием
+double pyramidVolume(double a, double h){
+    return a*a*h/3;
+}
+
+// ===== Главная функция: меню =====
+
+int main(){
+    system("chcp 65001");
+    int choice;
+    double a, h;
+    
+    do{
+        cout<< "\n=== Вариант 20: расчёты для пирамиды с квадратным основанием ===\n";
+        cout<< "1. Объём пирамиды\n";
+        cout<< "0. Выход\n";
+        cout<< "Выберите пункт: ";
+        cin>> choice;
+
+        switch(choice){
+            case 1:
+            do{
+                cout<<"Введите a и h: ";
+                cin>>a>>h;
+            } while(a<0||h<0);
+            cout<<"Объем = " << pyramidVolume(a,h)<<"\n";
+            break;
+
+            case 0:
+            cout<<"Работа завершена\n";
+            break;
+
+            default:
+            cout << "Такого пункта нет.\n";
+        }
+    }while(choice!=0);
+
+    return 0;
+}
